7ec2cf3c471c6744839291cd0747d46b44a0d406
Dave Kurman <dkurman@netsnbytes.com>
2018-03-07 15:27:32 -0500

    add type alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,4 +12,16 @@ $GitPromptSettings.DefaultPromptPrefix = '[$(hostname)] '
 $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
 
 Set-Alias ls Get-ChildItemColor -option AllScope -Force
+# Set-Alias ll Get-ChildItem -option AllScope -Force | Format-Wide
+
 Set-Alias dir Get-ChildItemColor -option AllScope -Force
+Set-Alias which gcm
+Set-Alias type Get-Content -option AllScope -Force
+
+function mkdir($foldername) { 
+    New-Item -ItemType Directory -Path $foldername
+}
+
+function ll() {
+    Get-ChildItem | Format-Wide
+}
