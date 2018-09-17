7488300d09fc84a5ef492f913e35939db63c8007
Dave Kurman <dkurman@netsnbytes.com>
2018-09-17 13:04:36 -0400

    Update ngen.ps1
    - Expand alias to full command for readability
    - Add comments with links for reference

--- ngen.ps1
+++ ngen.ps1
@@ -1,7 +1,16 @@
+# What is NGen?
+#
+#   NGen precompiles dotNet Common Intermediate Language code into machine code.
+#   This reduces startup times
+#
+# https://stackoverflow.com/questions/20620348/how-and-when-does-ngen-exe-work
+# https://docs.microsoft.com/en-us/dotnet/framework/tools/ngen-exe-native-image-generator
+#
+
 $env:path = [Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
-[AppDomain]::CurrentDomain.GetAssemblies() | % {
+[AppDomain]::CurrentDomain.GetAssemblies() | ForEach-Object {
   if (! $_.location) {continue}
   $Name = Split-Path $_.location -leaf
   Write-Host -ForegroundColor Yellow "NGENing : $Name"
-  ngen install $_.location | % {"`t$_"}
+  ngen install $_.location | ForEach-Object {"`t$_"}
 }
