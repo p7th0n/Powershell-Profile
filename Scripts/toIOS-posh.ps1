c2bb86346495f574be880512de32f900f708be61
Dave Kurman <dkurman@netsnbytes.com>
2018-11-07 20:16:54 -0500

    Move drafts scripts to modules

--- Scripts/toIOS-posh.ps1
+++
@@ -1,8 +0,0 @@
-$fromwindows = "C:\Users\Dave\Dropbox\notes\fromWindows.txt"
-$cmd = $Args -join " "
-$d2u = "dos2unix " + $fromwindows
-
-Write-Host $cmd
-iex $cmd
-iex $cmd *> $fromwindows
-iex $d2u
